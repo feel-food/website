a0459ea3404ff166b8e368cc4218fbd2b7b70759
Yann Lombard <contact@yannlombard.com>
2016-05-05 18:51:07 +0200

    watch svg folder updates

--- gulp/watch.js
+++ gulp/watch.js
@@ -10,6 +10,7 @@ module.exports = function(options) {
     // Watcher
     gulp.task('watch', function() {
         livereload.listen();
+        gulp.watch('*.svg', {cwd: params.tasksPath.source.icons}, ['icons']);
         gulp.watch('**/*.scss', {cwd: params.tasksPath.source.sassDir}, ['styles']);
         gulp.watch('**/*.js', {cwd: params.tasksPath.source.scriptsDir}, ['scripts']);
         gulp.watch('**/*', {cwd: params.tasksPath.source.imagesDir}, ['images']);
