a2d0f89c25ec5ad6de822879e5458d302b657d08
Yann Lombard <contact@yannlombard.com>
2016-05-05 11:25:45 +0200

    Rename "app" folder in "src" & index.html empty file creation

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,71 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var runSequence = require('run-sequence')
 
-var favicons = require("gulp-favicons"),
-    imagemin = require('gulp-imagemin'),
-    sass = require('gulp-sass'),
+var favicons     = require("gulp-favicons"),
+    imagemin     = require('gulp-imagemin'),
+    sass         = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    combineMq = require('gulp-combine-mq'),
-    cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    livereload = require('gulp-livereload');
+    combineMq    = require('gulp-combine-mq'),
+    cssnano      = require('gulp-cssnano'),
+    rename       = require('gulp-rename'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    livereload   = require('gulp-livereload');
 
-var params  = {
-    siteName: 'feel-food',
-    siteDesc: 'Feel Food',
-    appVersion: '1.0.0',
-    developerName: 'Yann Lombard',
-    developerURL: 'http://yannlombard.fr/',
-    faviconsPath: './favicon/',
-    faviconsIncludePath: 'app/_favicon.txt',
-    tasksPath: {
-        source: {
-            favicon: 'app/favicon.png',
-            images: 'app/images/**/*',
-            imagesDir: 'app/images/',
-            sass: 'app/sass/**/*.scss',
-            sassDir: 'app/sass/',
-            scripts: 'app/scripts/**/*.js',
-            scriptsDir: 'app/scripts/'
+var params = {
+    siteName           : 'feel-food',
+    siteDesc           : 'Feel Food',
+    appVersion         : '1.0.0',
+    developerName      : 'Yann Lombard',
+    developerURL       : 'http://yannlombard.fr/',
+    faviconsPath       : './favicon/',
+    faviconsIncludePath: 'src/_favicon.txt',
+    tasksPath          : {
+        source     : {
+            favicon   : 'src/favicon.png',
+            images    : 'src/images/**/*',
+            imagesDir : 'src/images/',
+            sass      : 'src/sass/**/*.scss',
+            sassDir   : 'src/sass/',
+            scripts   : 'src/scripts/**/*.js',
+            scriptsDir: 'src/scripts/'
         },
         destination: {
             favicon: 'dist/favicon/',
-            images: 'dist/images/',
-            css: 'dist/css/',
+            images : 'dist/images/',
+            css    : 'dist/css/',
             scripts: 'dist/js/'
         }
     }
 };
 
 // Favicon generation
-gulp.task('favicons', function () {
+gulp.task('favicons', function() {
     return gulp.src(params.tasksPath.source.favicon)
         .pipe(
             favicons({
-                appName: params.siteName, // Your application's name. `string`
-                background: '#fff', // Background colour for flattened icons. `string`
-                path: params.faviconsPath,      // Path for overriding default icons path. `string`
-                url: params.faviconsPath,       // Absolute URL for OpenGraph image. `string`
-                display: 'standalone', // Android display: "browser" or "standalone". `string`
+                appName    : params.siteName, // Your application's name. `string`
+                background : '#fff', // Background colour for flattened icons. `string`
+                path       : params.faviconsPath,      // Path for overriding default icons path. `string`
+                url        : params.faviconsPath,       // Absolute URL for OpenGraph image. `string`
+                display    : 'standalone', // Android display: "browser" or "standalone". `string`
                 orientation: 'portrait', // Android orientation: "portrait" or "landscape". `string`
-                version: params.appVersion, // Your application's version number. `number`
-                logging: false, // Print logs to console? `boolean`
-                online: false, // Use RealFaviconGenerator to create favicons? `boolean`
-                icons: {
-                    android: true,              // Create Android homescreen icon. `boolean`
-                    appleIcon: true,            // Create Apple touch icons. `boolean`
+                version    : params.appVersion, // Your application's version number. `number`
+                logging    : false, // Print logs to console? `boolean`
+                online     : false, // Use RealFaviconGenerator to create favicons? `boolean`
+                icons      : {
+                    android     : true,              // Create Android homescreen icon. `boolean`
+                    appleIcon   : true,            // Create Apple touch icons. `boolean`
                     appleStartup: true,         // Create Apple startup images. `boolean`
-                    coast: false,                // Create Opera Coast icon. `boolean`
-                    favicons: true,             // Create regular favicons. `boolean`
-                    firefox: true,              // Create Firefox OS icons. `boolean`
-                    opengraph: false,            // Create Facebook OpenGraph image. `boolean`
-                    twitter: false,              // Create Twitter Summary Card image. `boolean`
-                    windows: true,              // Create Windows 8 tile icons. `boolean`
-                    yandex: true                // Create Yandex browser icon. `boolean`
+                    coast       : false,                // Create Opera Coast icon. `boolean`
+                    favicons    : true,             // Create regular favicons. `boolean`
+                    firefox     : true,              // Create Firefox OS icons. `boolean`
+                    opengraph   : false,            // Create Facebook OpenGraph image. `boolean`
+                    twitter     : false,              // Create Twitter Summary Card image. `boolean`
+                    windows     : true,              // Create Windows 8 tile icons. `boolean`
+                    yandex      : true                // Create Yandex browser icon. `boolean`
                 },
-                html: params.faviconsIncludePath
+                html       : params.faviconsIncludePath
             })
         )
         .pipe(
@@ -74,7 +74,7 @@ gulp.task('favicons', function () {
 });
 
 // Image minification
-gulp.task('images', function () {
+gulp.task('images', function() {
     return gulp.src(params.tasksPath.source.images)
         .pipe(
             imagemin({
@@ -90,7 +90,7 @@ gulp.task('images', function () {
 });
 
 // SASS Compile with Autoprefixer
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src(params.tasksPath.source.sass)
         .pipe(
             sass().on('error', sass.logError)
@@ -121,8 +121,8 @@ gulp.task('sass', function () {
 });
 
 // Clean CSS
-gulp.task('cssclean', function () {
-    return gulp.src(params.tasksPath.destination.css+'styles.css')
+gulp.task('cssclean', function() {
+    return gulp.src(params.tasksPath.destination.css + 'styles.css')
         .pipe(
             combineMq({
                 beautify: true
@@ -145,7 +145,7 @@ gulp.task('cssclean', function () {
 });
 
 // Styles task
-gulp.task('styles', function(callback){
+gulp.task('styles', function(callback) {
     runSequence('sass', 'cssclean', callback);
 });
 
